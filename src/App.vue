d809d70a1a27c6426fdc9dc25fb3c2740d381137
Olexandr Filin <acfiln@ukr.net>
2024-05-18 15:27:43 +0300

    comm 2

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import IButton from './components/IButton/IButton.vue'
+const classes = {
+  bgcolor: false
+}
+</script>
+<template>
+  <span class="btn"> Hello span</span>
+  Hello vue <IButton :class="classes"></IButton>
+</template>
+
+<style>
+.bgcolor {
+  background-color: bisque;
+}
+</style>
---
+++ src/components/IButton/IButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class="btn">click me</button>
+</template>
+
+<style scoped>
+.btn {
+  color: rgb(16, 196, 166);
+  height: 120px;
+  width: 140px;
+}
+</style>
